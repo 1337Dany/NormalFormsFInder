71cf79cd4fdecfa5801bbefcee36bba09bb68dd3
Danial <dmunatayev.dev@gmail.com>
2024-10-17 11:37:29 +0200

    Fixed table

--- src/ShowNFTable.java
+++ src/ShowNFTable.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 
 public class ShowNFTable {
 
-    ArrayList<String> dependencyList = new ArrayList<>();
-
     public ShowNFTable() {
         showResult();
     }
@@ -18,10 +16,8 @@ public class ShowNFTable {
 
         String relation = String.valueOf(Relation.getRelation());
 
-        System.out.printf("%-" + columnWidth + "s%-" + columnWidth + "s%-" + columnWidth + "s%-" + columnWidth + "s%-"
-                        + columnWidth + "s%n",
-                relation,
-                "Trivial dep.", "X is a super key", "A is key attribute", "Not a pratial dep.");
+        System.out.printf("%-15s %-15s %-20s %-20s %-20s%n",
+                relation, "Trivial dep.", "X is a super key", "A is key attribute", "Not a pratial dep.");
 
         for (ArrayList<String> key : Dependency.getDependencies().keySet()) {
 
@@ -31,9 +27,7 @@ public class ShowNFTable {
             String value4 = NFFinder.AIsKeyAttribute(key) ? "T" : "F";
             String value5 = NFFinder.isPartialDependancy(key)? "T" : "F";
 
-                    System.out.printf("%" + (columnWidth + value1.length()) / 2 + "s%" + (columnWidth + value2.length()) / 2 + "s%"
-                            + (columnWidth + value3.length()) / 2 + "s%" + (columnWidth + value4.length()) / 2 + "s%"
-                            + (columnWidth + value5.length()) / 2 + "s%n", value1, value2, value3, value4, value5);
+                    System.out.printf("%-15s %-15s %-20s %-20s %-20s%n", value1,value2,value3,value4,value5);
 
         }
 
